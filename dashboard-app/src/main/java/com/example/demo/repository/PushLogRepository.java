198073a47671c05b58abc4d28662c827a4e92862
rlqja910 <kkb509@naver.com>
2026-08-22 15:03:23 +0900

    fix: remove java and python backend from repository

--- dashboard-app/src/main/java/com/example/demo/repository/PushLogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.PushLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PushLogRepository extends JpaRepository<PushLog, Long> {
-}
